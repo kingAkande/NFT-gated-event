d8b05b724fa1ee40af87f082d0526fc2d2d960a9
KingAkande <kingakande@gmail.com>
2024-09-14 22:29:20 +0100

    updated

---
+++ contracts/Erc721.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC721, Ownable {
+    constructor(address initialOwner)
+        ERC721("MyToken", "MTK")
+        Ownable(initialOwner)
+    {}
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+}
--- contracts/Nft-gated-Event.sol
+++ contracts/Nft-gated-Event.sol
@@ -1,72 +1,74 @@
-// SPDX-License-Identifier: M
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
-
-import "./IErc721.sol";
+import "./IErc721.sol" ;
 
 contract NftEvent {
 
-    address public owner;
-    uint256 public eventCount;
-    IERC721 public nftContract; // ERC-721 contract address to verify NFT ownership
+    address owner;
+    uint256 eventCount;
+    address tokenAddress;
 
-    constructor(address _nftContractAddress) {
+    constructor(address _tokenAddress){
+
         owner = msg.sender;
-        nftContract = IERC721(_nftContractAddress);
+        tokenAddress = _tokenAddress;
     }
 
     struct Event {
-        uint256 id;
-        string name;
-        string description;
-        string eventDate;
-        bool isOngoing;
-        address[] registeredUsers; // List of registered attendees
-    }
 
-    // Mapping of event ID to Event details
-    mapping (uint256 => Event) public events;
+    uint256 id;     
+    string name;
+    string description;
+    string eventDate;
+    bool isOngoing;
+    address[] registeredUsers;
 
-    // Mapping to keep track of which users registered for which events
-    mapping(address => mapping(uint256 => bool)) public isRegistered;
-
-    // Create a new event (only for NFT holders)
-    function createEvent(
-        string memory _name,
-        string memory _description,
-        string memory _eventDate,
-        bool _isOngoing
-    ) external {
-        require(nftContract.balanceOf(msg.sender) > 0, "Not an NFT holder"); // Ensure the caller holds an NFT
-        
-        eventCount++;
-    
-       
-
-        events[eventCount] = Event({
-            id: eventCount,
-            name: _name,
-            description: _description,
-            eventDate: _eventDate,
-            isOngoing: _isOngoing,
-            registeredUsers.push(msg.sender)  // Initialize with empty list
-            
-        });
-    }
-
-    // Register as an attendee for a specific event (only for NFT holders)
-    function registerAttendees(uint256 _eventId) external {
-        require(nftContract.balanceOf(msg.sender) > 0, "Not an NFT holder"); // Ensure the caller holds an NFT
-        require(_eventId > 0 && _eventId <= eventCount, "Invalid event ID");
-        require(!isRegistered[msg.sender][_eventId], "Already registered");
-
-        // Register the user for the event
-        events[_eventId].registeredUsers.push(msg.sender);
-        isRegistered[msg.sender][_eventId] = true;
-    }
-
-    // View registered users for an event
-    function getRegisteredUsers(uint256 _eventId) external view returns (address[] memory) {
-        require(_eventId > 0 && _eventId <= eventCount, "Invalid event ID");
-        return events[_eventId].registeredUsers;
-    }
 }
+
+ 
+    mapping(address => bool) hasNFT;
+
+    mapping (uint256 => Event) events;
+    mapping(address => mapping(uint256 => bool)) hasRegistered;
+
+    function createEvent(string memory _name , string memory _description , string memory _eventDate ) external {
+
+    uint256 _eventID = eventCount + 1;  /* unique ID to look up event */
+    
+    Event storage evT = events[_eventID];
+    evT.id = _eventID;
+    evT.name = _name;
+    evT.description = _description;
+    evT.eventDate =_eventDate;
+    evT.registeredUsers.push(msg.sender);
+    hasRegistered[msg.sender][_eventID] = true;
+
+    eventCount +=1 ;
+
+    }
+
+ function registerAttendees(uint256 _eventID) external {
+
+    require (IERC721(tokenAddress).balanceOf(msg.sender) > 0 , "not an nft holder");
+    require(_eventID != 0 , "not in existence");
+    require(_eventID <= eventCount , "not in existence");
+    require( !hasRegistered[msg.sender][_eventID] , "Already registered");
+    events[_eventID].registeredUsers.push(msg.sender);
+    hasRegistered[msg.sender][_eventID] = true;
+
+ }
+
+
+ function getAregisteredUsers(uint256 _eventID) external view returns (address[] memory){
+
+        require( _eventID >0  && _eventID <= eventCount,"invalid" );
+
+
+        return events[_eventID].registeredUsers;
+ }
+    
+
+
+}
+
+
